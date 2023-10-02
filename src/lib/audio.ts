94d2742da3c20182a6e714f643aae4dd6e2c0875
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-10-02 03:04:10 -0700

    Move RingBuffer inside AudioWorklet string

--- src/lib/audio.ts
+++ src/lib/audio.ts
@@ -26,48 +26,46 @@ function uint8ArrayToFloat32Array(u8a: Uint8Array) {
   return f32Array;
 }
 
-class RingBuffer {
-  buffer: Float32Array;
-  writePointer: number;
-  readPointer: number;
-  availableData: number;
-
-  constructor(length: number) {
-    this.buffer = new Float32Array(length);
-    this.writePointer = 0;
-    this.readPointer = 0;
-    this.availableData = 0;
-  }
-
-  push(data: Float32Array) {
-    for (let i = 0; i < data.length; i++) {
-      let sample = data[i];
-      if (this.availableData >= this.buffer.length) return; // Buffer full
-      this.buffer[this.writePointer] = sample;
-      this.writePointer = (this.writePointer + 1) % this.buffer.length;
-      this.availableData++;
-    }
-  }
-
-  pull(amount: number) {
-    let output = new Float32Array(amount);
-    for (let i = 0; i < amount; i++) {
-      if (this.availableData <= 0) {
-        output.set(new Float32Array(amount - i), i); // Fill the rest with zeros
-        break;
-      }
-      output[i] = this.buffer[this.readPointer];
-      this.readPointer = (this.readPointer + 1) % this.buffer.length;
-      this.availableData--;
-    }
-    return output;
-  }
-}
-
 async function setupAudioModule() {
   const processorCode = `
           // Incorporate RingBuffer inside the AudioWorkletProcessor
-          ${RingBuffer.toString()}
+          class RingBuffer {
+            buffer: Float32Array;
+            writePointer: number;
+            readPointer: number;
+            availableData: number;
+
+            constructor(length: number) {
+              this.buffer = new Float32Array(length);
+              this.writePointer = 0;
+              this.readPointer = 0;
+              this.availableData = 0;
+            }
+
+            push(data: Float32Array) {
+              for (let i = 0; i < data.length; i++) {
+                let sample = data[i];
+                if (this.availableData >= this.buffer.length) return; // Buffer full
+                this.buffer[this.writePointer] = sample;
+                this.writePointer = (this.writePointer + 1) % this.buffer.length;
+                this.availableData++;
+              }
+            }
+
+            pull(amount: number) {
+              let output = new Float32Array(amount);
+              for (let i = 0; i < amount; i++) {
+                if (this.availableData <= 0) {
+                  output.set(new Float32Array(amount - i), i); // Fill the rest with zeros
+                  break;
+                }
+                output[i] = this.buffer[this.readPointer];
+                this.readPointer = (this.readPointer + 1) % this.buffer.length;
+                this.availableData--;
+              }
+              return output;
+            }
+          }
 
           class BufferedPlayerProcessor extends AudioWorkletProcessor {
               constructor() {
@@ -140,7 +138,6 @@ function pushBase64Audio(
 export {
   base64ToUint8Array,
   uint8ArrayToFloat32Array,
-  RingBuffer,
   setupAudioModule,
   pushBase64Audio,
 };
