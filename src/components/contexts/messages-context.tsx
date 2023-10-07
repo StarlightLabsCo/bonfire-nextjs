88f4a0e7aa938fba9f136f76a39746cde2ba1c09
Ryan Elliott <rselliott99@gmail.com>
2023-10-07 17:09:42 -0400

    Add undo and delete-messages

--- src/components/contexts/messages-context.tsx
+++ src/components/contexts/messages-context.tsx
@@ -10,21 +10,16 @@ type MessageLike = Message | { id: string; role: string; content: string };
 type MessagesContextValue = {
   messages: Array<MessageLike>;
   setMessages: React.Dispatch<React.SetStateAction<Array<MessageLike>>>;
-  suggestions: Array<string>;
-  setSuggestions: React.Dispatch<React.SetStateAction<Array<string>>>;
 };
 
 export const MessagesContext = createContext<MessagesContextValue>({
   messages: [],
   setMessages: () => {},
-  suggestions: [],
-  setSuggestions: () => {},
 });
 
 export function MessagesProvider({ children }: { children: React.ReactNode }) {
   const { socket } = useContext(WebSocketContext);
   const [messages, setMessages] = useState<MessageLike[]>([]);
-  const [suggestions, setSuggestions] = useState<string[]>([]);
 
   useEffect(() => {
     if (socket) {
@@ -79,6 +74,12 @@ export function MessagesProvider({ children }: { children: React.ReactNode }) {
               return messages;
             }
           });
+        } else if (data.type === WebSocketResponseType['delete-messages']) {
+          setMessages((messages) => {
+            return messages.filter((message) => {
+              return !data.payload.ids.includes(message.id);
+            });
+          });
         } else if (data.type === WebSocketResponseType.image) {
           setMessages((messages) => {
             const messageExists = messages.some(
@@ -107,16 +108,23 @@ export function MessagesProvider({ children }: { children: React.ReactNode }) {
             }
           });
         } else if (data.type === WebSocketResponseType.suggestions) {
-          setSuggestions(data.payload.content);
+          setMessages((messages) => {
+            return [
+              ...messages,
+              {
+                id: data.payload.id,
+                role: 'function',
+                content: data.payload.content,
+              },
+            ];
+          });
         }
       });
     }
   }, [socket]);
 
   return (
-    <MessagesContext.Provider
-      value={{ messages, setMessages, suggestions, setSuggestions }}
-    >
+    <MessagesContext.Provider value={{ messages, setMessages }}>
       {children}
     </MessagesContext.Provider>
   );
--- src/lib/websocket-schema.ts
+++ src/lib/websocket-schema.ts
@@ -2,6 +2,7 @@ enum WebSocketResponseType {
   'instance',
   'message',
   'message-append',
+  'delete-messages',
   'image',
   'suggestions',
   'audio',
