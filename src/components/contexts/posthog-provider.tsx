b8ead04bce8628399de26fefb3e25c8731eda925
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-10-21 15:37:50 -0700

    Update posthog

--- src/components/contexts/posthog-provider.tsx
+++ src/components/contexts/posthog-provider.tsx
@@ -11,7 +11,6 @@ if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: 'https://app.posthog.com',
-    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     loaded: (posthog) => {
       if (process.env.NODE_ENV === 'development') posthog.debug();
     },
