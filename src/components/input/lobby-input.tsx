cf09ed6a340e312869d2ee2b716e0cfdc77b96d5
Harris Rothaermel <harris.rothaermel@gmail.com>
2023-10-25 23:23:05 -0700

    Update suggestions timeout.

--- src/components/input/lobby-input.tsx
+++ src/components/input/lobby-input.tsx
@@ -51,7 +51,7 @@ export function LobbyInput({
           type: 'generateAdventureSuggestions',
           payload: {},
         });
-      }, 250);
+      }, 1000);
     }
   }, [adventureSuggestions, sendJSON, socketState]);
 
